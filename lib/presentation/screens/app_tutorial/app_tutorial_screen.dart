d3d733431f64561ed275095834167936d227ef7c
Juan Carlos Bautista <jcbautista2701@gmail.com>
2023-07-10 00:05:20 -0500

    Screens creadas y barril actualizado

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTutorialScreen extends StatelessWidget {
+  static const String name = 'tutorial_screen';
+  const AppTutorialScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScrollScreen extends StatelessWidget {
+  static const String name = 'infinite_screen';
+  const InfiniteScrollScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
